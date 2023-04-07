50d0e1a3b1da64cafb0769592dac8ec31ee4a394
jakiasultana2022 <rokeyasbd@gmail.com>
2023-04-07 12:44:46 +0600

    add profile image

---
+++ app/components/MyProfilePic.tsx
@@ -0,0 +1,18 @@
+
+
+const MyProfilePic = () => {
+    return (
+        <section className="w-full mx-auto">
+        <img
+            className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
+            src="https://images.unsplash.com/photo-1484399172022-72a90b12e3c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            width={200}
+            height={200}
+            alt="Rokeya Sultana"
+           
+        />
+    </section>
+    );
+};
+
+export default MyProfilePic;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import MyProfilePic from './components/MyProfilePic'
 import Navbar from './components/Navbar'
 import './globals.css'
 
@@ -15,6 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="dark:bg-slate-800">
         <Navbar></Navbar>
+        <MyProfilePic></MyProfilePic>
         {children}
       
       </body>
